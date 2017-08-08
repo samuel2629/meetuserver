350fb42fdb4bca2b9043bb6f62148b92fd365030
Samuel <samuel2629@gmail.com>
2017-08-08 02:25:12 +0200

    api

--- src/main/java/com/silho/ideo/meetuserver/Controller/MeetusController.java
+++ src/main/java/com/silho/ideo/meetuserver/Controller/MeetusController.java
@@ -14,6 +14,7 @@ public class MeetusController {
     }
 
     @RequestMapping(value = "/apiKey", method = RequestMethod.GET)
+    @ResponseBody
     public String getApiKey(){
         return "AIzaSyDuioRSRLgLzgBwGGaY3qPln411JJhRUIA";
     }

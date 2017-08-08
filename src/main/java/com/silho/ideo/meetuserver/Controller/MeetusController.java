74708cb26657deebe30d7acd2f3a17525b04dc4b
Samuel <samuel2629@gmail.com>
2017-08-08 17:03:19 +0200

    api

--- src/main/java/com/silho/ideo/meetuserver/Controller/MeetusController.java
+++ src/main/java/com/silho/ideo/meetuserver/Controller/MeetusController.java
@@ -3,12 +3,12 @@ package com.silho.ideo.meetuserver.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MeetusController {
 
-    private String apiKey = System.getenv("API_KEY");
 
     @RequestMapping("/")
     public String home(){
@@ -17,6 +17,16 @@ public class MeetusController {
 
     @RequestMapping(value = "/apiKey", method = RequestMethod.GET)
     public String getApiKey(){
-        return apiKey;
+        return System.getenv("API_KEY");
+    }
+
+    @RequestMapping(value = "/request", method = RequestMethod.POST)
+    @ResponseBody
+    public String request(@RequestParam("duration") String duration,
+                          @RequestParam("myLatitude") double myLatitude,
+                          @RequestParam("myLongitude") double myLongitude,
+                          @RequestParam("latitudeDestination") double latitudeDestination,
+                          @RequestParam("longitudeDestination") double longitudeDestination){
+        return duration + myLatitude + myLongitude + latitudeDestination + longitudeDestination;
     }
 }

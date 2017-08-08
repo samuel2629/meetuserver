861b8831cf276451b8d595b166144ac39adf4aac
Samuel <samuel2629@gmail.com>
2017-08-08 02:23:20 +0200

    api

--- src/main/java/com/silho/ideo/meetuserver/Controller/MeetusController.java
+++ src/main/java/com/silho/ideo/meetuserver/Controller/MeetusController.java
@@ -2,6 +2,7 @@ package com.silho.ideo.meetuserver.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,5 +11,10 @@ public class MeetusController {
     @RequestMapping("/")
     public String home(){
         return "home";
+    }
+
+    @RequestMapping(value = "/apiKey", method = RequestMethod.GET)
+    public String getApiKey(){
+        return "AIzaSyDuioRSRLgLzgBwGGaY3qPln411JJhRUIA";
     }
 }

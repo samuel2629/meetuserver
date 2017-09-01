58b408c4873c9fb6670cd087ff86a4c0c9d1252c
Samuel <samuel2629@gmail.com>
2017-09-01 17:10:35 +0200

    api

---
+++ src/main/java/com/silho/ideo/meetuserver/model/User.java
@@ -0,0 +1,73 @@
+package com.silho.ideo.meetuserver.model;
+
+/**
+ * Created by Samuel on 01/08/2017.
+ */
+
+public class User {
+
+    public String token, idFacebook, name, profilPic;
+    public double latitude;
+    public double longitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getIdFacebook() {
+        return idFacebook;
+    }
+
+    public void setIdFacebook(String idFacebook) {
+        this.idFacebook = idFacebook;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfilPic() {
+        return profilPic;
+    }
+
+    public void setProfilPic(String profilPic) {
+        this.profilPic = profilPic;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+
+    public User(){}
+
+    public User(String token, double myLatitude, double myLongitude, String idFacebook, String name, String profilPic){
+        this.token = token;
+        this.latitude = myLatitude;
+        this.longitude = myLongitude;
+        this.idFacebook = idFacebook;
+        this.name = name;
+        this.profilPic = profilPic;
+
+    }
+}

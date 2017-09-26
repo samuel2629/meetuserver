035575551dbf7936f35e66bfd034933415fd02c0
Samuel <samuel2629@gmail.com>
2017-09-26 17:00:06 +0200

    api

--- src/main/java/com/silho/ideo/meetuserver/Controller/MeetusController.java
+++ src/main/java/com/silho/ideo/meetuserver/Controller/MeetusController.java
@@ -60,7 +60,7 @@ public class MeetusController {
                 body.put("registration_ids", registration_ids);
                 body.put("priority", "high");
 
-                // body.put("dry_run", true);
+                body.put("dry_run", true);
 
                 JSONObject data = new JSONObject();
                 data.put("latitudeDestination", latitudeDestination);
